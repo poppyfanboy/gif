b6d80d9f8a35b43fbd0b5f1fd5a34861a9fd6413
poppyfanboy <poppyfanboy69@gmail.com>
2025-04-02 14:48:44 +0300

    Configure stb_image to use an arena
    Probably a bad idea, because arena here wastes quite a bunch of memory
    without giving any benefits back. (The cost of calling malloc here is
    way too low in comparison to the useful work getting done.)
    
    I just wanted to see, if it would be possible to come up with an
    interface that does not depend on the specific arena struct type. You
    might want this in case you have multiple independent libraries and each
    of them uses its own type for the arena, but within the calling code you
    want to use a single arena for all of them.
    
    Just using a "void *" as the arena type and accessing the arena data at
    pre-agreed offsets might be a better solution?

--- image_load.c
+++ image_load.c
@@ -1,31 +1,76 @@
 #include "image_load.h"
 
+#include <stddef.h> // size_t, NULL
+#include <string.h> // memcpy
+#include <assert.h> // assert
+
+#define ARENA_ALIGNMENT 16
+
+static ImageLoadArena *current_arena = NULL;
+
+static void *arena_alloc(size_t size) {
+    assert(current_arena != NULL);
+
+    isize padding = (-((uptr) current_arena->begin)) & (ARENA_ALIGNMENT - 1);
+    isize memory_left = current_arena->end - current_arena->begin - padding;
+    if (memory_left < 0 || (size_t) memory_left < size) {
+        // stbi_load will exit gracefully and return NULL, if we fail to allocate memory.
+        return NULL;
+    }
+
+    void *ptr = current_arena->begin + padding;
+    current_arena->begin += padding + (isize) size;
+    return ptr;
+}
+
+static void arena_free(void *ptr) {
+    assert(current_arena != NULL);
+
+    // Cannot do anything special here, because stb_image does not give us the allocation size.
+    (void) ptr;
+}
+
+static void *arena_realloc(void *old_ptr, size_t old_size, size_t new_size) {
+    assert(current_arena != NULL);
+
+    if ((u8 *) old_ptr + old_size < current_arena->begin) {
+        void *new_ptr = arena_alloc(new_size);
+        memcpy(new_ptr, old_ptr, old_size);
+        return new_ptr;
+    } else {
+        // Try to extend an old allocation, if it happens to be the topmost one.
+        // (Doesn't seem to happen in practice with stbi_load.)
+        current_arena->begin -= old_size;
+        return arena_alloc(new_size);
+    }
+}
+
+#define STBI_MALLOC(size) arena_alloc(size)
+#define STBI_REALLOC_SIZED(ptr, old_size, new_size) arena_realloc(ptr, old_size, new_size)
+#define STBI_FREE(ptr) arena_free(ptr)
+
 #define STBI_NO_LINEAR
-#define STBI_NO_HDR
+#define STBI_ONLY_PNG
+#define STBI_ONLY_JPEG
+#define STB_IMAGE_STATIC
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-bool image_load(char const *file_name, Image *image) {
-    int image_width;
-    int image_height;
-    int image_bytes_per_pixel;
-    u8 *image_data = stbi_load(file_name, &image_width, &image_height, &image_bytes_per_pixel, 0);
-    if (image == NULL) {
-        return false;
+u8 *image_load(char const *file_name, isize *width_out, isize *height_out, ImageLoadArena *arena) {
+    current_arena = arena;
+    ImageLoadArena arena_checkpoint = *arena;
+
+    int width = 0;
+    int height = 0;
+    int channel_count;
+    u8 *pixels = stbi_load(file_name, &width, &height, &channel_count, IMAGE_BYTES_PER_PIXEL);
+    if (pixels == NULL) {
+        *arena = arena_checkpoint;
     }
 
-    image->data = image_data;
-    image->width = image_width;
-    image->height = image_height;
-    image->bytes_per_pixel = image_bytes_per_pixel;
+    *width_out = width;
+    *height_out = height;
 
-    return true;
-}
-
-isize image_size(Image const *image) {
-    return image->width * image->height * image->bytes_per_pixel;
-}
-
-void image_destroy(Image *image) {
-    stbi_image_free(image->data);
+    current_arena = NULL;
+    return pixels;
 }
